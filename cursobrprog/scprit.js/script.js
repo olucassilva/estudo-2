e2401503e5528f0f81ff1290eb6c934f28b89115
Lucas Silva <lucasnaito@gmail.com>
2021-10-27 22:59:09 -0300

    aula

---
+++ cursobrprog/scprit.js/script.js
@@ -0,0 +1,35 @@
+//Boolean = é true or false =operação logica
+//null = variavel vazia
+//undefined = não definido
+//number = 1;
+//string = "1";
+ 
+var a = " sajbdkshdaskj";
+
+console.log(a)
+
+//opeaçoes Aritméticas
+
+// + adição
+// _ subtração
+// * multiplicação
+// / divisao
+// % módulo
+// ++ incremento
+// -- decremento
+
+
+var a = 30;
+
+
+var b = a++;
+console.log (b);
+
+
+//=   x = y  x=y
+// += x += y x = x + y
+// -= x-=y   x = x - y 
+// *= x *= y x = x * y
+// /= x /= y x = x / y
+// %= x%= y x = x % 
+
