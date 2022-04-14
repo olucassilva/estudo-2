d22417c75471f571e92e4ed93fdb7bc5ce873dcc
Lucas Silva <lucasnaito@gmail.com>
2022-04-14 04:13:16 -0300

    aulajs

---
+++ cursojs-master/aula_11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 157.0 
+console.log (`a velocidade do seu carro é ${vel} km/h`)
+if (vel> 60 )  //condição simples !
+ { 
+  console.log (`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log (`Se Beber, não dirija`)    
---
+++ cursojs-master/aula_11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+console.log(`vivendo em ${país}`)
+
+if (país == 'Brasil') {
+  console.log ('voce é Brasileiro')
+
+ } else {
+   console.log ('vc é Estrangeiro')
+ }
