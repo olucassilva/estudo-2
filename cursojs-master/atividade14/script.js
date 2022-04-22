a6e2fb4ae79f99253f41294eb64b6fa69ece4c03
Lucas Silva <lucasnaito@gmail.com>
2022-04-22 02:46:21 -0300

    ex15js

--- cursojs-master/atividade14/script.js
+++ cursojs-master/atividade14/script.js
@@ -2,9 +2,9 @@ function carregar() {
   var msg = window.document.getElementById ('msg')
   var img = window.document.getElementById ('imagem')
   var data = new Date()
-  var hora = 10
+  var hora = 18
  // var hora = data.getHours()
-  msg.innerHTML = `Agora são ${hora} horas.` 
+  msg.innerHTML = (`Agora são ${hora} horas.`) 
   if (hora >= 0 && hora < 12) {
        //Bom Dia!
        img.src = 'fotomanha.png'
