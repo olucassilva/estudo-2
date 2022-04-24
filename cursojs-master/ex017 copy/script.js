337c3b2664a6e9f3779b670a83678d1b863a0f90
Lucas Silva <lucasnaito@gmail.com>
2022-04-24 03:47:51 -0300

    atividadejs

---
+++ cursojs-master/ex017 copy/script.js
@@ -0,0 +1,14 @@
+function tabuada() {
+  let num = window.document.getElementById('txtn')
+  let tab = window.document.getElementById ('seltab')
+ if (num.value.length == 0) {
+  window.alert ('Por Favor, digite um Número') 
+ } else {
+    let n = Number (num.value)
+    let d = 1
+    while (c <= 10) {
+      let 
+    }
+ }
+
+}
