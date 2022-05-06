fe81efadaa87cb26751dfbffdb2480031be12cc8
Lucas Silva <lucasnaito@gmail.com>
2022-05-05 23:57:11 -0300

    aulajs

--- cursojs-master/ex017 copy/script.js
+++ cursojs-master/ex017 copy/script.js
@@ -2,12 +2,16 @@ function tabuada() {
   let num = window.document.getElementById('txtn')
   let tab = window.document.getElementById ('seltab')
  if (num.value.length == 0) {
-  window.alert ('Por Favor, digite um Número') 
+  window.alert ('Por Favor, digite um Número!') 
  } else {
-    let n = Number (num.value)
-    let d = 1
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ''
     while (c <= 10) {
-      let 
+      let item = window.document.createElement('option')
+      item.text = `${n} x ${c} = ${n*c}`
+      tab.appendChild(item)
+      c++
     }
  }
 
